3d96573094b0b9308f792d947d2806a90830c3cc
Akshita Laddha <117356670+akshitaladdha27@users.noreply.github.com>
2025-06-19 20:26:13 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
 
-// Middleware
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
-app.use(express.json()); // to parse JSON bodies
+app.use(express.json()); 
 
-// In-memory product list
 let products = [
   { id: '1', name: 'Ring', price: 999, imageUrl: '/ring.webp' },
   { id: '2', name: 'Necklace', price: 1599, imageUrl: '/necklace.jpg' },
   { id: '3', name: 'Bangles', price: 1999, imageUrl: '/bangles.webp' },
 ];
 
-
-// GET all products
 app.get('/products', (req, res) => {
   res.json(products);
 });
 
-// POST new product
 app.post('/products', (req, res) => {
   const { name, price, imageUrl } = req.body;
   const newProduct = {
@@ -33,7 +29,6 @@ app.post('/products', (req, res) => {
   res.status(201).json(newProduct);
 });
 
-// DELETE product by id
 app.delete('/products/:id', (req, res) => {
   const { id } = req.params;
   const index = products.findIndex(p => p.id === id);
@@ -44,7 +39,7 @@ app.delete('/products/:id', (req, res) => {
   res.status(200).json(deleted[0]);
 });
 
-// Start server
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
